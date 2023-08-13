76bc0ab43f3adbde5ecf8885eed4a5f3d49ecf1f
zyryc15 <zyryc15@gmail.com>
2023-08-14 01:12:43 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,21 @@ import ChatFeed from "./components/ChatFeed";
 import LoginForm from "./components/LoginForm";
 
 function App() {
-  // if (!localStorage.getItem("username")) return <LoginForm />;
+  if (!localStorage.getItem("username")) return <LoginForm />;
 
   return (
-    <LoginForm />
-    // <ChatEngine
-    //   height="100vh"
-    //   projectID={projectID}
-    //   userName={localStorage.getItem("username")}
-    //   userSecret={localStorage.getItem("password")}
-    //   renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-    //   onNewMessage={() =>
-    //     new Audio(
-    //       "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
-    //     ).play()
-    //   }
-    // />
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
   );
 }
 
