b61d714dd037b52738ed72aa830be216cfa6c4c4
zyryc15 <zyryc15@gmail.com>
2023-08-13 23:07:31 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="526addee-1232-4083-8cbf-11c877f7e1d9"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+}
+
+export default App;
